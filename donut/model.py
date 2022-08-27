bbdbac164d1b5d4bafedcabccdbbca3511d522df
Nguyễn Anh Bình <binhna1@fsoft.com.vn>
2022-08-28 01:10:17 +0700

    change base revision

--- donut/model.py
+++ donut/model.py
@@ -596,7 +596,7 @@ class DonutModel(PreTrainedModel):
                 Name of a pretrained model name either registered in huggingface.co. or saved in local,
                 e.g., `naver-clova-ix/donut-base`, or `naver-clova-ix/donut-base-finetuned-rvlcdip`
         """
-        model = super(DonutModel, cls).from_pretrained(pretrained_model_name_or_path, revision="official", *model_args,
+        model = super(DonutModel, cls).from_pretrained(pretrained_model_name_or_path, revision="main", *model_args,
                                                        **kwargs)
 
         # truncate or interplolate position embeddings of donut decoder
